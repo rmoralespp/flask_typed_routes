71dbd7251e15c489352a9faeca8c5eb7b33f31b9
Rolando Morales <rolando.morales@wisetechglobal.com>
2024-12-19 18:51:27 +0100

    Get OpenAPI schema Route parameters.

---
+++ flask_typed_routes/openapi.py
@@ -0,0 +1,65 @@
+import collections
+
+import pydantic
+
+import flask_typed_routes.errors as ftr_errors
+import flask_typed_routes.fields as ftr_fields
+
+ref_template = "#/components/schemas/{model}"
+
+
+def get_parameters(data, fields):
+    """
+    Get OpenAPI schema Route parameters.
+
+    :param data: Python dictionary with the Pydantic model schema.
+    :param Iterable[flask_typed_routes.fields.Field] fields: Field definitions
+    :return:
+    """
+
+    required = set(data.get("required", ()))
+    definitions = data.get("$defs", dict())
+    schemas = dict()
+    kinds = (
+        ftr_fields.FieldTypes.path,
+        ftr_fields.FieldTypes.query,
+        ftr_fields.FieldTypes.cookie,
+        ftr_fields.FieldTypes.header,
+    )
+    for name, schema in data["properties"].items():
+        if "$ref" in schema:
+            reference = schema["$ref"].split("/")[-1]  # basename
+            definition = definitions[reference]
+            if name in required:
+                required.remove(name)
+            required.update(definition.get("required", ()))
+            schemas.update(definition["properties"])
+        else:
+            schemas[name] = schema
+
+    params = collections.defaultdict(dict)
+    for field in fields:
+        if field.kind in kinds:
+            slot = params[field.kind]
+
+            if issubclass(field.annotation, pydantic.BaseModel):
+                names = [info.alias or name for name, info in field.annotation.model_fields.items()]
+            else:
+                names = [field.locator]
+            for name in names:
+                if name in slot:
+                    msg = f"Duplicate parameter: [name={name}, in={field.kind}]"
+                    raise ftr_errors.InvalidParameterTypeError(msg)
+                else:
+                    schema = schemas[name]
+                    slot[name] = {
+                        "name": name,
+                        "description": schema.pop("title"),
+                        "in": field.kind,
+                        "required": name in required,
+                        "schema": schema,
+                    }
+
+    for _, value in params.items():
+        for _, schema in value.items():
+            yield schema
---
+++ tests/unit/test_openapi.py
@@ -0,0 +1,65 @@
+import typing as t
+
+import pydantic
+
+import flask_typed_routes.fields as ftr_fields
+import flask_typed_routes.openapi as ftr_openapi
+
+# Path field
+a = ftr_fields.Path()
+a.name = "a"
+a.annotation = str
+# Query field
+b = ftr_fields.Query()
+b.name = "b"
+b.annotation = str
+
+
+class QueryParams(pydantic.BaseModel):
+    inner: t.Annotated[str, pydantic.Field(alias="inner-alias", max_length=3)]
+    outer: t.Annotated[str, pydantic.Field(title="Outer Title", max_length=3)]
+
+
+# Embedded Query field
+c = ftr_fields.Query()
+c.name = "c"
+c.annotation = QueryParams
+
+
+def test_get_parameters():
+    model = pydantic.create_model("model", a=(str, ...), b=(str, ...), c=(QueryParams, ...))
+    data = model.model_json_schema(ref_template=ftr_openapi.ref_template)
+    fields = [a, b, c]
+
+    expected = (
+        {
+            'description': 'A',
+            'in': 'path',
+            'name': 'a',
+            'required': True,
+            'schema': {'type': 'string'},
+        },
+        {
+            'description': 'B',
+            'in': 'query',
+            'name': 'b',
+            'required': True,
+            'schema': {'type': 'string'},
+        },
+        {
+            'description': 'Inner-Alias',
+            'in': 'query',
+            'name': 'inner-alias',
+            'required': True,
+            'schema': {'maxLength': 3, 'type': 'string'},
+        },
+        {
+            'description': 'Outer Title',
+            'in': 'query',
+            'name': 'outer',
+            'required': True,
+            'schema': {'maxLength': 3, 'type': 'string'},
+        },
+    )
+    result = tuple(ftr_openapi.get_parameters(data, fields))
+    assert result == expected
