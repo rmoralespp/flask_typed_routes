9cd8c7a51722ffd203a236a0dac25541334a2a63
Rolando Morales <rolando.morales@wisetechglobal.com>
2025-01-16 13:50:51 +0100

    Added more tests

--- tests/unit/test_openapi.py
+++ tests/unit/test_openapi.py
@@ -1,4 +1,3 @@
-import inspect
 import unittest.mock
 
 import pydantic
@@ -99,9 +98,7 @@ def test_get_request_body_ref():
     class Model(pydantic.BaseModel):
         pass
 
-    fields = [
-        unittest.mock.Mock(locator="body", kind="body", annotation=Model)
-    ]
+    fields = [unittest.mock.Mock(locator="body", kind="body", annotation=Model)]
     model_properties = {"body": {"$ref": "#/components/schemas/Model"}}
     model_required_fields = ["field"]
     result = ftr_openapi.get_request_body(fields, model_properties, model_required_fields)
@@ -109,9 +106,7 @@ def test_get_request_body_ref():
         'content': {
             'application/json': {
                 'schema': {
-                    'properties': {
-                        'body': {'$ref': '#/components/schemas/Model'}
-                    },
+                    'properties': {'body': {'$ref': '#/components/schemas/Model'}},
                     'required': [],
                     'type': 'object',
                 }
@@ -148,4 +143,95 @@ def test_get_request_body_obj():
         },
         'required': True,
     }
+    assert result == expected
+
+
+def test_get_request_body_no_fields():
+    result = ftr_openapi.get_request_body([], {}, [])
+    assert result is None
+
+
+def test_openapi_init_default():
+    expected = {
+        'components': None,
+        'contact_info': None,
+        'description': None,
+        'external_docs': None,
+        'license_info': None,
+        'openapi_version': '3.1.0',
+        'paths': {},
+        'security': None,
+        'servers': None,
+        'summary': None,
+        'tags': None,
+        'terms_of_service': None,
+        'title': 'API doc',
+        'version': '0.0.0',
+        'webhooks': None,
+    }
+    assert ftr_openapi.OpenApi().__dict__ == expected
+
+
+def test_openapi_init_filled():
+    params = {
+        'components': object(),
+        'contact_info': object(),
+        'description': object(),
+        'external_docs': object(),
+        'license_info': object(),
+        'openapi_version': '3.1.0',
+        'security': object(),
+        'servers': object(),
+        'summary': object(),
+        'tags': object(),
+        'terms_of_service': object(),
+        'title': object(),
+        'version': object(),
+        'webhooks': object(),
+    }
+    expected = {**params, 'paths': {}}
+    assert ftr_openapi.OpenApi(**params).__dict__ == expected
+
+
+def test_openapi_models_json_schema():
+    class Model1(pydantic.BaseModel):
+        field1: str
+        field2: str
+
+    class Model2(pydantic.BaseModel):
+        field3: str
+        field4: str
+        embed: list[Model1]
+
+    result = ftr_openapi.OpenApi.models_json_schema([Model1, Model2])
+    expected = (
+        {
+            (Model1, 'validation'): {'$ref': '#/components/schemas/Model1'},
+            (Model2, 'validation'): {'$ref': '#/components/schemas/Model2'},
+        },
+        {
+            '$defs': {
+                'Model1': {
+                    'properties': {
+                        'field1': {'title': 'Field1', 'type': 'string'},
+                        'field2': {'title': 'Field2', 'type': 'string'},
+                    },
+                    'required': ['field1', 'field2'],
+                    'title': 'Model1',
+                    'type': 'object',
+                },
+                'Model2': {
+                    'properties': {
+                        'embed': {'items': {'$ref': '#/components/schemas/Model1'}, 'title': 'Embed', 'type': 'array'},
+                        'field3': {'title': 'Field3', 'type': 'string'},
+                        'field4': {'title': 'Field4', 'type': 'string'},
+                    },
+                    'required': ['field3', 'field4', 'embed'],
+                    'title': 'Model2',
+                    'type': 'object',
+                },
+            }
+        },
+    )
+
     assert result == expected
